750375b8e9021d3119afb77e44276f400d3d6c3f
Arpan Sharma <arpansharma@my.unt.edu>
2019-10-05 16:24:12 -0500

    Success

---
+++ Init.java
@@ -0,0 +1,12 @@
+class Init{
+    public static void main(String args[]){
+    Child n = new Child();
+    
+    parent p = new Child();
+    n.counter =2;
+    System.out.println(n.counter);
+    n.declare();
+    p.eat();
+    
+
+}}
